d7849a75a8f717ca4d0ea185286195032921f974
Tiago <2092015@my.ipleiria.pt>
2018-12-21 21:24:36 +0000

    Removed table error

--- resources/js/components/tableList.vue
+++ resources/js/components/tableList.vue
@@ -6,14 +6,14 @@
         </tr>
         </thead>
         <tbody>
-        <tr v-for="table in tables"  :key="table.id">
-            <td>{{ table.table_number }}</td>
+        <tr v-for="restaurant_table in tables"  :key="restaurant_table.id">
+            <td>{{ restaurant_table.table_number }}</td>
         </tr>
         </tbody>
     </table>
 <!--    <b-container class="bv-example-row">
         <b-row >
-            <b-col class=".col-md-150" v-for="table in tables"  :key="table.id">{{ table.table_number }}</b-col>
+            <b-col class=".col-md-150" v-for="restaurant_table in tables"  :key="restaurant_table.id">{{ restaurant_table.table_number }}</b-col>
             <b-col>2 of 3</b-col>
             <b-col>3 of 3</b-col>
         </b-row>
