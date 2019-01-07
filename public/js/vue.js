e134d21a3e2c14e215c70402da57687a4c6156a7
Tiago <2092015@my.ipleiria.pt>
2019-01-07 00:06:45 +0000

    Permissions and sht

--- public/js/vue.js
+++ public/js/vue.js
@@ -25753,20 +25753,29 @@ var login = Vue.component('login', __webpack_require__(134));
 var register = Vue.component('register', __webpack_require__(17));
 var addMeal2 = Vue.component('addMeal2', __webpack_require__(21));
 
-var routes = [{ path: '/', redirect: '/mainComponent' }, { path: '/mainComponent', component: mainComponent }, { path: '/users', component: user }, { path: '/items', component: item }, { path: '/orders', component: order }, { path: '/meals', component: meal }, { path: '/tables', component: restaurantTable }, { path: '/invoices', component: invoice }, { path: '/profile', component: profile, name: 'profile' }, { path: '/login', component: login, name: 'login' }, { path: '/register', component: register, name: 'register' }, { path: '/addMeal2', component: addMeal2, name: 'addMeal2' }];
+var routes = [{ path: '/', component: item }, { path: '/users', component: user,
+    beforeEnter: function beforeEnter(to, from, next) {
+        if (__WEBPACK_IMPORTED_MODULE_0__stores_global_store__["a" /* default */].state.user.type == 'manager') {
+            next();
+        } else {
+            next(false);
+        }
+    } }, { path: '/orders', component: order }, { path: '/meals', component: meal }, { path: '/tables', component: restaurantTable }, { path: '/invoices', component: invoice }, { path: '/profile', component: profile, name: 'profile' }, { path: '/login', component: login, name: 'login' }, { path: '/register', component: register, name: 'register' }, { path: '/addMeal2', component: addMeal2, name: 'addMeal2' }];
 
 var router = new __WEBPACK_IMPORTED_MODULE_1_vue_router__["a" /* default */]({
     routes: routes
 });
 
 router.beforeEach(function (to, from, next) {
-    if (to.name == 'mainComponent' || to.name == 'logout') {
+    if (to.path !== '/') {
         if (!__WEBPACK_IMPORTED_MODULE_0__stores_global_store__["a" /* default */].state.user) {
-            next("/login");
-            return;
+            next('/');
+        } else {
+            next();
         }
+    } else {
+        next();
     }
-    next();
 });
 
 var app = new Vue({
@@ -25774,6 +25783,7 @@ var app = new Vue({
     store: __WEBPACK_IMPORTED_MODULE_0__stores_global_store__["a" /* default */],
     created: function created() {
         this.$store.commit('loadTokenAndUserFromSession');
+
         //console.log(this.$store.state.user);
     }
 }).$mount('#app');
@@ -51910,9 +51920,10 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
             axios.get('api/users').then(function (response) {
                 _this5.users = response.data.data;
+            }).catch(function (error) {
+                console.log("ERROR");
             });
         }
-
     },
     mounted: function mounted() {
         this.getUsers();
@@ -53680,6 +53691,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
+    name: "order",
     data: function data() {
         return {
             title: 'List Orders',
@@ -55811,6 +55823,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
+//
 
 
 
@@ -55845,7 +55858,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
         isLogged: function isLogged() {
             // `this` points to the vm instance
             return this.$store.state.user;
-            console.log(this.$store.state.user);
         },
         typeUser: function typeUser() {
             if (!this.$store.state.user) {
@@ -55895,7 +55907,7 @@ var render = function() {
       )
     ]),
     _vm._v(" "),
-    _c("div", [_c("item", { attrs: { items: _vm.items } })], 1)
+    _c("div", [_c("router-view")], 1)
   ])
 }
 var staticRenderFns = [
@@ -56036,17 +56048,21 @@ if (false) {
 /***/ (function(module, exports, __webpack_require__) {
 
 var disposed = false
+function injectStyle (ssrContext) {
+  if (disposed) return
+  __webpack_require__(141)
+}
 var normalizeComponent = __webpack_require__(0)
 /* script */
 var __vue_script__ = __webpack_require__(135)
 /* template */
-var __vue_template__ = __webpack_require__(136)
+var __vue_template__ = __webpack_require__(143)
 /* template functional */
 var __vue_template_functional__ = false
 /* styles */
-var __vue_styles__ = null
+var __vue_styles__ = injectStyle
 /* scopeId */
-var __vue_scopeId__ = null
+var __vue_scopeId__ = "data-v-172b28a4"
 /* moduleIdentifier (server only) */
 var __vue_module_identifier__ = null
 var Component = normalizeComponent(
@@ -56086,6 +56102,22 @@ module.exports = Component.exports
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__profile_vue__ = __webpack_require__(23);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__profile_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__profile_vue__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 //
 //
 //
@@ -56178,7 +56210,57 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 });
 
 /***/ }),
-/* 136 */
+/* 136 */,
+/* 137 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 138 */,
+/* 139 */,
+/* 140 */,
+/* 141 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(142);
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add the styles to the DOM
+var update = __webpack_require__(2)("6bbdb163", content, false, {});
+// Hot Module Replacement
+if(false) {
+ // When the styles change, update the <style> tags
+ if(!content.locals) {
+   module.hot.accept("!!../../../node_modules/css-loader/index.js!../../../node_modules/vue-loader/lib/style-compiler/index.js?{\"vue\":true,\"id\":\"data-v-172b28a4\",\"scoped\":true,\"hasInlineConfig\":true}!../../../node_modules/vue-loader/lib/selector.js?type=styles&index=0!./login.vue", function() {
+     var newContent = require("!!../../../node_modules/css-loader/index.js!../../../node_modules/vue-loader/lib/style-compiler/index.js?{\"vue\":true,\"id\":\"data-v-172b28a4\",\"scoped\":true,\"hasInlineConfig\":true}!../../../node_modules/vue-loader/lib/selector.js?type=styles&index=0!./login.vue");
+     if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+     update(newContent);
+   });
+ }
+ // When the module is disposed, remove the <style> tags
+ module.hot.dispose(function() { update(); });
+}
+
+/***/ }),
+/* 142 */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(1)(false);
+// imports
+
+
+// module
+exports.push([module.i, "\nimg[data-v-172b28a4] {\n    width: 30px;\n}\n", ""]);
+
+// exports
+
+
+/***/ }),
+/* 143 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var render = function() {
@@ -56307,9 +56389,34 @@ var render = function() {
         ? _c(
             "div",
             [
-              this.$store.state.user
+              _c("a", { staticClass: "dropdown-item" }, [
+                _c("img", { attrs: { src: this.$store.state.user.photo_url } }),
+                _vm._v(
+                  "\n            " +
+                    _vm._s(this.$store.state.user.username) +
+                    "\n        "
+                )
+              ]),
+              _vm._v(" "),
+              this.$store.state.user.shift_active
                 ? _c("a", { staticClass: "dropdown-item" }, [
-                    _vm._v(_vm._s(this.$store.state.user.username))
+                    _c("label", [_vm._v("Turno atual:")]),
+                    _vm._v(" "),
+                    _c("p", { staticClass: "text-10" }, [
+                      _vm._v(
+                        "\n                " +
+                          _vm._s(this.$store.state.user.last_shift_start) +
+                          " - " +
+                          _vm._s(this.$store.state.user.last_shift_end) +
+                          "\n            "
+                      )
+                    ])
+                  ])
+                : _vm._e(),
+              _vm._v(" "),
+              this.$store.state.user.shift_active
+                ? _c("a", { staticClass: "dropdown-item" }, [
+                    _c("label", [_vm._v("Fim do turno:")])
                   ])
                 : _vm._e(),
               _vm._v(" "),
@@ -56368,12 +56475,6 @@ if (false) {
     require("vue-hot-reload-api")      .rerender("data-v-172b28a4", module.exports)
   }
 }
-
-/***/ }),
-/* 137 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
